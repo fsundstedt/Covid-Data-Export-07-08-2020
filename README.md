1b0521759eb1cc94789d38ed18c02d2c91b432ac
unknown <frank.sundstedt@gmail.com>
2020-07-08 18:24:35 -0400

    app functional. data aggregation and exporting works. Basic README included

---
+++ README.md
@@ -0,0 +1,10 @@
+# Covid Data Table
+
+Simple application pulls data from the following api: 
+https://about-corona.net/documentation
+
+It aggregates select data, and uses Pandas to export it to an excel file.
+
+## Excel FIle Location
+
+Excel files are named according to the date of the first data entry provided, and are saved in the same folder as the application. This can be changed in line 40.
---
+++ covid-data-export.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import urllib, urllib.request, json
+
+url = 'https://corona-api.com/countries'
+
+response = urllib.request.urlopen(url)
+
+data = json.loads(response.read())
+
+country_data_raw = data['data']
+
+date = (country_data_raw[0]['updated_at'])[0:10]
+
+country_data = []
+
+for country in country_data_raw:
+    single_country = {}
+    single_country['name'] = country['name']
+    single_country['population'] = country['population']
+    single_country['deaths'] = country['latest_data']['deaths']
+    single_country['total_cases'] = country['latest_data']['confirmed']
+    single_country['death_rate'] = (country['latest_data']['calculated']['death_rate'])
+
+    if str(country['latest_data']['calculated']['death_rate'])[0].isdigit():
+        single_country['death_rate_%'] = round((country['latest_data']['calculated']['death_rate']), 2)
+    else:
+        single_country['death_rate_%'] = 'N/A'
+
+    if str(country['latest_data']['confirmed'])[0].isdigit() and str(country['population'])[0].isdigit():
+        single_country['population_infected_rate_%'] = round((country['latest_data']['confirmed'] / country['population'] * 100), 3)
+    else:
+        single_country['population_infected_rate_%'] = 'N/A'
+
+    single_country['date_updated'] = country['updated_at']
+    country_data.append(single_country)
+
+df = pd.DataFrame(country_data)
+
+file_name = './covid_data_' + date + '.xlsx'
+
+df.to_excel(file_name)
