e05fcc68018189d319742260d58fc79398875b6b
unknown <frank.sundstedt@gmail.com>
2020-07-16 22:38:23 -0400

    WIP. adding additional formatting

--- covid-data-export.py
+++ covid-data-export.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import urllib, urllib.request, json
+from xlsxwriter.utility import xl_rowcol_to_cell
 
 url = 'https://corona-api.com/countries'
 
@@ -17,7 +18,11 @@ country_data = []
 for country in country_data_raw:
     single_country = {}
     single_country['name'] = country['name']
-    single_country['population'] = country['population']
+    if single_country['name'] == 'USA':
+        single_country['population'] = 329964713
+    else:
+        single_country['population'] = country['population']
+
     single_country['deaths'] = country['latest_data']['deaths']
     single_country['total_cases'] = country['latest_data']['confirmed']
 
@@ -26,13 +31,13 @@ for country in country_data_raw:
     else:
         single_country['infected_death_rate_%'] = 'N/A'
 
-    if str(country['latest_data']['confirmed'])[0].isdigit() and str(country['population'])[0].isdigit():
-        single_country['population_infected_rate_%'] = round((country['latest_data']['confirmed'] / country['population'] * 100), 3)
+    if str(country['latest_data']['confirmed'])[0].isdigit() and str(single_country['population'])[0].isdigit():
+        single_country['population_infected_rate_%'] = round((country['latest_data']['confirmed'] / single_country['population'] * 100), 3)
     else:
         single_country['population_infected_rate_%'] = 'N/A'
 
-    if str(country['latest_data']['deaths'])[0].isdigit() and str(country['population'])[0].isdigit():
-        single_country['population_death_rate_%'] = round((country['latest_data']['deaths'] / country['population'] * 100), 4)
+    if str(country['latest_data']['deaths'])[0].isdigit() and str(single_country['population'])[0].isdigit():
+        single_country['population_death_rate_%'] = round((country['latest_data']['deaths'] / single_country['population'] * 100), 4)
     else:
         single_country['population_death_rate_%'] = 'N/A'
 
@@ -41,6 +46,15 @@ for country in country_data_raw:
 
 df = pd.DataFrame(country_data)
 
-file_name = './covid_data_' + date + '.xlsx'
+file_name = './data_sets/covid_data_' + date + '.xlsx'
 
-df.to_excel(file_name)
+writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+df.to_excel(writer, index=True, sheet_name='data')
+
+workbook = writer.book
+worksheet = writer.sheets['data']
+worksheet.set_column('B:B', 30)
+worksheet.set_column('C:H', 15)
+worksheet.set_column('I:I', 23)
+
+writer.save()
